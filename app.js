923eb25a59b206b0cc217616c587c63cf2277302
Kathleen Kent <lashkath@gmail.com>
2015-09-14 16:42:51 -0700

    updates

--- app.js
+++ app.js
@@ -1,16 +1,28 @@
 var express = require('express');
 var app = express();
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var url = require('url');
 app.locals.users = [];
-var body-parser = 
+
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
 
 app.get('/', function (req, res) {
   res.sendfile('/Users/kathleen/myapp/microblog/login.html');
 });
 
-app.post('/login', function (req, res) {
-	var username = req.params.user;
-	req.app.locals.users.push(username);
-	console.log(username)
+// app.post('/login/:user', function (req, res) {
+// 	var username = req.params.user;
+// 	req.app.locals.users.push(username);
+// 	//res.end();
+// 	//console.log(username)
+// })
+
+app.post('/login', function (req, res){	
+	//console.log(req)
+	console.log(req.body)
 })
 
 var server = app.listen(3000, function () {
